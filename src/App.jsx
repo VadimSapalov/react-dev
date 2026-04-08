e5b35c43ccac9eda7bacb96bd8c0802cc5d769af
Tori_II <vadimsapalev@gmail.com>
2026-04-08 17:47:14 +0300

    update app to have cart items and price count
    + we now have bootstrap and use useState to count

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
+import { useState } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header'
 import Main from './components/Main'
 import Footer from './components/Footer'
 import './App.css'
 
 function App() {
-  
+  const [cartCount, setCartCount] = useState(0);
+  const [cartPriceCount, setcartPriceCount] = useState(0);
+  const addToCartCallback = (price) => {
+    setCartCount((cartCount) => cartCount + 1);
+    setcartPriceCount((cartPriceCount) => cartPriceCount + (price));
+  }
   return (
     <div className='app-wraper'>
-      <Header />
-      <Main />
+      <Header itemCount = {cartCount} price = {cartPriceCount}/>
+      <Main onAddToCart = {addToCartCallback}/>
       <Footer />
     </div>
   );
