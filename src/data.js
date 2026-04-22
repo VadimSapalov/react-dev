f6644cdba10104048cda048939228ae5782d541a
Tori_II <vadimsapalev@gmail.com>
2026-04-22 18:43:24 +0300

    Create data.js

---
+++ src/data.js
@@ -0,0 +1,6 @@
+export const softwares = [
+  {id: 1, title: 'Crimson Desert', description: 'Ipsum lorem at boreale', price: 70},
+  {id: 2, title: 'Helldirevs', description: 'qwertytrewq  qwertyuytrewqwerty', price: 30},
+  {id: 3, title: 'Stellar Blade', description: 'qwertyuytrewqqwertyu', price: 50},
+  {id: 4, title: 'Wuthering Waves', description: 'qqwertyu', price: 0},
+]
