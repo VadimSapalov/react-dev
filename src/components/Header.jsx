5e6000c0145e60c5d45aab01657de90963faf54a
Tori_II <vadimsapalev@gmail.com>
2026-04-22 18:44:51 +0300

    update header with router links

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,26 @@
-function Header ({itemCount, price}) {
+import { Link, NavLink } from 'react-router-dom'
+
+function Header ({ itemCount, price }) {
   return (
-    <header className="header">
-      <nav>
-        <ul className="nav-list">
-          <li><a href="#" className="nav-link">Головна</a></li>
-          <li><a href="#" className="nav-link">Каталог</a></li>
-          <li><a href="#" className="nav-link">Про нас</a></li>
-          <li>Кошик: ({itemCount})</li>
-          <li>Загальна ціна: ({price})</li>
-        </ul>
-      </nav>
-    </header>
-  );
-};
+    <nav className="navbar navbar-expand-lg navbar-light bg-light px-4">
+      <Link className="navbar-brand" to="/">MyGameStore</Link>
+      <ul className="navbar-nav me-auto">
+        <li className="nav-item">
+          <NavLink className="nav-link" to="/">Головна</NavLink>
+        </li>
+        <li className="nav-item">
+          <NavLink className="nav-link" to="/catalog">Бібліотека</NavLink>
+        </li>
+        <li className="nav-item">
+          <NavLink className="nav-link" to="/about">Про нас</NavLink>
+        </li>
+      </ul>
+
+      <span className="navbar-text">
+       Кошик ({itemCount}) | {price}$
+      </span>
+    </nav>
+  )
+}
 
 export default Header;
